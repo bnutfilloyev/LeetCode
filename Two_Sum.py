1108e674cb1fa7844f7fe418d180ec658f87e9df
Bexruz Nutfilloyev <yoshlikmedia@gmail.com>
2021-10-28 08:10:38 -0400

    Add new solution

---
+++ Two_Sum.py
@@ -0,0 +1,12 @@
+
+def twoSum(nums, target):
+    print(nums)
+    for i in range(len(nums)):
+        for  j in range(i, len(nums)):
+            if nums[i] + nums[j] == target and i != j:
+                return [i, j]
+            elif i == len(nums) and j == len(nums):
+                return []
+
+
+print(twoSum([3,2,4], 6))
