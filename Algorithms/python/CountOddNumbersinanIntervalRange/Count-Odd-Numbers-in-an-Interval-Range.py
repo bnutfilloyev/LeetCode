4e2e08b1e62a61c2b57ff8e208415df3bf0f8a03
yoshlikmedia <yoshlikmedia@gmail.com>
2022-06-17 23:00:46 +0500

    2053. Kth Distinct String in an Array 1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ Algorithms/python/CountOddNumbersinanIntervalRange/Count-Odd-Numbers-in-an-Interval-Range.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countOdds(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        return (high + 1) / 2 - low / 2
